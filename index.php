902868ffb9b319114f33e5dd64787f1b779d4642
Shogo Tabata <shogotabata@ShogonoMacBook-Pro.local>
2017-10-07 15:05:08 +0900

    msg

--- index.php
+++ index.php
@@ -14,6 +14,7 @@ foreach ($events as $event) {
 
   if ($event instanceof \LINE\LINEBot\Event\MessageEvent) {
     if ($event instanceof \LINE\LINEBot\Event\MessageEvent\TextMessage) {
+      
       if($event->getText() === 'こんにちは') {
         $bot->replyMessage($event->getReplyToken(),
           (new \LINE\LINEBot\MessageBuilder\MultiMessageBuilder())
@@ -26,6 +27,28 @@ foreach ($events as $event) {
             ->add(new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('「こんにちは」と呼びかけて下さいね！'))
             ->add(new \LINE\LINEBot\MessageBuilder\StickerMessageBuilder(1, 4))
         );
+      }
+    }else if($event instanceof \LINE\LINEBot\Event\MessageEvent\ImageMessage){
+      $response = $bot->getMessageContent($event->getMessageId());
+      $rawBody = $response->getRawBody();
+      $im = imagecreatefromstring($rawBody);
+      $resultString = "";
+      if ($im !== false) {
+      $filename = uniqid();
+      $directory_path = "tmp";
+      if(!file_exists($directory_path)) {
+        if(mkdir($directory_path, 0777, true)) {
+            chmod($directory_path, 0777);
+        }
+      }
+      imagejpeg($im, $directory_path. "/" . $directory_path . '/' . $filename . ".jpg", 75);
+      } else {
+      $resultString = "upload failed";
+
+      $bot->replyMessage($event->getReplyToken(),
+          (new \LINE\LINEBot\MessageBuilder\MultiMessageBuilder())
+            ->add(new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('saved at ' ."http://" . $_SERVER["HTTP_HOST"] . "/" . $directory_path . '/' . $filename . ".jpg"))
+            ->add(new \LINE\LINEBot\MessageBuilder\StickerMessageBuilder(1, 4))
       }
     }
     continue;
